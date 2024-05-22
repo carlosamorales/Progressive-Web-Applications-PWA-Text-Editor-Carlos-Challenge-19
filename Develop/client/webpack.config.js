5abaf8306d80a79b546a777cedcc990d27dc5490
Carlos Asencio Morales <carlosgkt@gmail.com>
2024-05-22 18:10:37 -0500

    updating the path in the webpack.config.js and adding babel in the root package.json

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,24 +3,26 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-module.exports = () => {
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
   return {
-    mode: 'production',
+    mode: isProduction ? 'production' : 'development',
     entry: {
-      main: path.resolve(__dirname, 'client/src/js/index.js'),
-      install: path.resolve(__dirname, 'client/src/js/install.js')
+      main: path.resolve(__dirname, 'src/js/index.js'),
+      install: path.resolve(__dirname, 'src/js/install.js')
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'client/dist'),
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'client/index.html'),
+        template: path.resolve(__dirname, 'index.html'),
         title: 'J.A.T.E'
       }),
-      new InjectManifest({
-        swSrc: path.resolve(__dirname, 'client/src-sw.js'),
+      isProduction && new InjectManifest({
+        swSrc: path.resolve(__dirname, 'src-sw.js'),
         swDest: 'service-worker.js',
       }),
       new WebpackPwaManifest({
@@ -35,13 +37,13 @@ module.exports = () => {
         publicPath: '/',
         icons: [
           {
-            src: path.resolve(__dirname, 'client/src/images/logo.png'),
+            src: path.resolve(__dirname, 'src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
         ],
       }),
-    ],
+    ].filter(Boolean),
     module: {
       rules: [
         {
@@ -55,11 +57,17 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime'],
             },
           },
         },
       ],
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      compress: true,
+      port: 8080,
+      open: true,
     },
   };
 };

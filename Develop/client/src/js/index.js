221d0e9df6e40c0ef0af0da26f362e839d324ab6
Carlos Asencio Morales <carlosgkt@gmail.com>
2024-05-22 14:34:20 -0500

    Add the event handlers for PWA installation,Update your src-sw.js to cache assets,update database.js and setup IndexedDB,Update index.js to Use IndexedDB Functions,add the install functionality in install.js, add install button to index.html

--- Develop/client/src/js/index.js
+++ Develop/client/src/js/index.js
@@ -11,7 +11,7 @@ const loadSpinner = () => {
   spinner.classList.add('spinner');
   spinner.innerHTML = `
   <div class="loading-container">
-  <div class="loading-spinner" />
+    <div class="loading-spinner"></div>
   </div>
   `;
   main.appendChild(spinner);
@@ -26,7 +26,7 @@ if (typeof editor === 'undefined') {
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
   // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
+  const workboxSW = new Workbox('/service-worker.js');
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
